a77d8bcafd170baa19358d68904b01a1b4cb34f0
Louie <dustyfingers01257@gmail.com>
2025-01-11 22:30:09 -0700

    create user using gql mutation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { useQuery, useMutation, gql } from "@apollo/client";
+import { useState } from "react";
 
 const GET_USERS = gql`
   query GetUsers {
@@ -23,25 +24,57 @@ const GET_USER_BY_ID = gql`
   }
 `;
 
+const CREATE_USER = gql`
+  mutation CreateUser($name: String!, $age: Int!, $isMarried: Boolean!) {
+    createUser(name: $name, age: $age, isMarried: $isMarried) {
+      name
+    }
+  }
+`;
+
 function App() {
+  const [newUser, setNewUser] = useState({
+    name: "",
+    age: null,
+    isMarried: false,
+  });
   const {
     data: getUsersData,
     error: getUsersError,
     loading: getUsersLoading,
   } = useQuery(GET_USERS);
-  const {
-    data: getUserByIdData,
-    error: getUserByIdError,
-    loading: getUserByIdLoading,
-  } = useQuery(GET_USER_BY_ID, {
-    variables: {
-      id: "2",
-    },
-  });
+  const { data: getUserByIdData, loading: getUserByIdLoading } = useQuery(
+    GET_USER_BY_ID,
+    {
+      variables: {
+        id: "2",
+      },
+    }
+  );
+  const [createUser] = useMutation(CREATE_USER);
+  const handleCreateUser = async () => {
+    createUser({ variables: { ...newUser, age: Number(newUser.age) } });
+  };
   if (getUsersLoading) return <p>Data loading...</p>;
   if (getUsersError) return <p>Error fetching data: {getUsersError.message}</p>;
   return (
     <>
+      <div>
+        <input
+          placeholder="Name"
+          onChange={(evt) =>
+            setNewUser((currUser) => ({ ...currUser, name: evt.target.value }))
+          }
+        />
+        <input
+          placeholder="Age"
+          type="number"
+          onChange={(evt) =>
+            setNewUser((currUser) => ({ ...currUser, age: evt.target.value }))
+          }
+        />
+        <button onClick={() => handleCreateUser()}>Create User</button>
+      </div>
       <div>
         {getUserByIdLoading ? (
           <p>Loading chosen user...</p>
